4b23ecd73baa4d7cbc108d65c44256c9006f2486
Raquel Olaya <raquelolaya@Raquels-MacBook-Air.local>
2022-05-18 09:09:43 -0500

    keep going

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+const port = 3000;
+
+
+
+
+
+app.listen(port, (req, res) => {
+    console.log(req)
+    res.send(`Listening on port`, port)
+
+});
+
