f38c46d46d957991ffc820a5e1de2088b4b49e61
Raquel Olaya <raquelolaya@Raquels-MacBook-Air.local>
2022-05-25 18:20:13 -0500

    hope this works

--- server.js
+++ server.js
@@ -1,52 +1,42 @@
 const express = require("express");
 const app = express();
-const drinks = require('./modles/drinks');
-const food = require('./modles/drinks');
+const drinks = require("./models/drinks");
+const food = require("./models/food");
 const port = 3000;
 
 // start port at 3000
 app.get("/", (req, res) => {
-    res.send(`Welcome to the Gitpub App! Go to <a herf="/pub">localhost3000/pub</a> to get started!`)
+  res.send(`Welcome to the Gitpub App!`);
 });
 
 // index
-app.get("/drinks", (req, res) => {
-    res.render('drinks_index.ejs', {totalDrinks : drinks})
+app.get("/drinks", (req, res) => { 
+  console.log('anything', drinks);
+  res.render("drinks_index.ejs", { totalDrinks: drinks });
 });
 
-app.get('/food', (req, res) => {
-    res.render('food_index.ejs', {totalFood : food})
+app.get("/food", (req, res) => {
+  res.render("food_index.ejs", { totalFood: food });
 });
 
-app.get("/drink/:id", (req, res) => {
-    res.render(req.params.id);
-});
+// app.get("/drinks/:id", (req, res) => {
+//   res.render(req.params.id);
+// });
 
-app.get('/food/:id', (req, res) => {
-    res.render(req.params.id);
-});
+// app.get("/food/:id", (req, res) => {
+//   res.render(req.params.id);
+// });
 
 app.get("/drinks/:id", (req, res) => {
-    res.render("drinks_show.ejs", {drinks: drinks[req.params.id]});
+  res.render("drinks_show.ejs", { drinks: drinks[req.params.id] });
 });
 
 app.get("/food/:id", (req, res) => {
-    res.render("food_show.ejs", {food: food[req.params.id]});
+  res.render("food_show.ejs", { food: food[req.params.id] });
 });
 
-
-app.get('/drinks/:id', (req, res) => {
-    res.render('drinks_show.ejs', {drink: drinks[req.params.id]});
-});
-
-
-app.get("/food/:id", (req, res) => {
-    res.render("food_show.ejs", {food: food[req.params.id]});
-});
 
 // listen to me dang it !
 app.listen(port, (req, res) => {
-    console.log(`Listening on port ${port}`)
-
+  console.log(`Listening on port ${port}`);
 });
-
